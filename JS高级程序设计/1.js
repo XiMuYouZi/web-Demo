4223a0cbe5dc75373f379c55888d2063b94c181a
wwty <>
2021-01-22 13:45:41 +0800

    保存下

--- JS高级程序设计/1.js
+++ JS高级程序设计/1.js
@@ -1,10 +1,10 @@
 
 let myName = "Tom";
 let myAge = 20;
-let myfn = function(){
+let myfn = function () {
     return "1.js: My name is" + myName + "! I'm '" + myAge + "years old."
 }
-let myClass =  class myClass {
+let myClass = class myClass {
     static a = "yeah!";
 }
 let defaultExport = "defaultExport"
